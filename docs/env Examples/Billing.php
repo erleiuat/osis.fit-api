c6824f18ddddecac8cf7f0494b97f56b59ecd3b9
Elia <mail@eliareutlinger.ch>
2019-08-08 13:37:06 +0200

    Update Billing.php

--- docs/env Examples/Billing.php
+++ docs/env Examples/Billing.php
@@ -2,8 +2,8 @@
 
 class Env_bill {
 
-    const cb_site = "osis-fit-test";
-    const cb_tkn = "test_bEyMPSgEjzZx4cn7q1avZiPwp6XtPOSx";
+    const cb_site = "test-site";
+    const cb_tkn = "test_xyz";
     const plan = "premium";
 
 }
