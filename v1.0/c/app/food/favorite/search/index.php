91f48d0c64a209fe104666ec62111650babc50ab
Elia <mail@eliareutlinger.ch>
2019-08-15 12:04:46 +0200

    Update index.php

--- v1.0/c/app/food/favorite/search/index.php
+++ v1.0/c/app/food/favorite/search/index.php
@@ -14,20 +14,60 @@ require_once ROOT . 'Security.php'; /* Load Security-Methods */
 try {
 
     $sec = Sec::auth($_LOG);
+    $data = Core::getBody(['query' => ['string', true]]);
 
     if(!$sec->premium) throw new ApiException(401, 'premium_required');
 
-    $items = [
-        [
-            "title" => "Blabla",
-            "image" => false,
-            "caloriesPer100" => 55,
-            "calories" => 123,
-        ]
-    ];
+    $search = $data->query;
+    $url = "https://world.openfoodfacts.org/cgi/search.pl?search_terms=".$search."&search_simple=1&action=process&json=1";
+    $response = file_get_contents($url);
 
-    $_REP->addData(count($items), "total");
-    $_REP->addData($items, "items");
+    $response = json_decode($response);
+
+    $arr = [];
+
+    foreach($response->products as $product){
+        if(isset($product->nutriments)) {
+            $nut = $product->nutriments;
+
+            $serving_size = 100;
+            $energy_unit = null;
+
+            $energy_100g = null;
+            $energy = null;
+            $energy_serving = null;
+
+            if (isset($product->serving_size)) $serving_size = intval($product->serving_size);
+            if (isset($nut->energy_unit)) $energy_unit = $nut->energy_unit;
+
+            if (isset($nut->energy_100g)) $energy_100g = $nut->energy_100g;
+            else break;
+
+            if($energy_unit === "kJ") {
+                $energy_100g = round($energy_100g / 4.184, 2);
+            }
+
+            $caloriesPer100 = $energy_100g;
+            $amount = $serving_size;
+            $calories = round($serving_size * ($energy_100g/100), 2);
+
+            $tmp = [
+                "code" => $product->code,
+                "title" => $product->product_name,
+                "image" => $product->image_url,
+                "caloriesPer100" => $caloriesPer100,
+                "amount" => $amount,
+                "calories" => $calories
+            ];
+
+            array_push($arr, $tmp);
+
+        }
+
+    }
+
+    $_REP->addData(count($arr), "total");
+    $_REP->addData($arr, "items");
 
 } catch (\Exception $e) { 
     Core::processException($_REP, $_LOG, $e); 
